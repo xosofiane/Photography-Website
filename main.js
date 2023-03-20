f1adad5425c6a71ec9503c2504229ec80acc45f8
xosofiane <sofiane11messi@gmail.com>
2023-03-20 16:05:04 +0100

    all sections are done but needs pics changes

--- main.js
+++ main.js
@@ -12,3 +12,22 @@ textButtons.forEach(textButton => {
     text.innerHTML = text.innerHTML.split('').map((character, index) => 
     `<span style="transform: rotate(${index * 11}deg)">${character}</span>`).join('');
 })
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    599 : {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    1023 : {
+      slidesPerView: 3,
+      spaceBetween: 60
+    }
+  }
+});
